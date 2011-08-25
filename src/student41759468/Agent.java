588feff979c012ba4a22b0eb5eda13c961ab1c4f
Emile Victor <emilevictor@gmail.com>
2011-08-25 12:36:00 +1000

    parseInput being modified LIKE A BOSS

--- src/student41759468/Agent.java
+++ src/student41759468/Agent.java
@@ -2,12 +2,20 @@ package student41759468;
 
 import java.util.StringTokenizer;
 import datastructures.*;
-
+import java.io.*;
+/**
+ * Agent class
+ * 
+ * Allows you to deal with selling and buying data.
+ * 
+ * @author Emile Victor
+ *
+ */
 public class Agent implements IAgent {
 
-	private IREPLACEME<Stock> buyOrders;
-	private IREPLACEME<Stock> sellOrders;
-	private IREPLACEME<Stock> transactions;
+	private LinkedList<Stock> buyOrders;
+	private LinkedList<Stock> sellOrders;
+	private LinkedList<Stock> transactions;
 
 	/*
 	 * Default constructor
@@ -15,27 +23,72 @@ public class Agent implements IAgent {
 	public Agent() {
 		// You may choose which data structures you would like to use
 
-		this.buyOrders = new REPLACEME<Stock>();
-		this.sellOrders = new REPLACEME<Stock>();
-		this.transactions = new REPLACEME<Stock>();
+		this.buyOrders = new LinkedList<Stock>();
+		this.sellOrders = new LinkedList<Stock>();
+		this.transactions = new LinkedList<Stock>();
 	}
 
-	/*
+	/**
 	 * Takes a file name as input and parses the commands in the file
 	 */
 	public int parseInput(String fileName) {
-		// Implement this method
+		//		String currentDir = new File(".").getAbsolutePath();
+		String lineReadFromBuffer;
+		String simpleRegexDelimeter = " ";
+		//		System.out.println(currentDir);
+		
+		/**
+		 * Kludgey POS: adding /src/test to the file path in order to
+		 * stop the FileNotFound exception in the try/catch below.
+		 */
+		StringBuilder stringBuilder = new StringBuilder(fileName);
+		stringBuilder.insert(1, "/src/test");
+		fileName = stringBuilder.toString();
+		
+		/**
+		 * Regex for this function (perhaps):
+		 * ^(buy|sell)\s\w{4}?\s\d+?\s[$]\d+?[.]\d{0,2}$
+		 */
+		
+		try {
+			/**
+			 * Create a file input stream, data input stream from fileinput stream
+			 * and then finally a buffered reader to get the lines out of the file.
+			 */
+			FileInputStream fs = new FileInputStream(fileName);
+			DataInputStream in = new DataInputStream(fs);
+			BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+				
+			/**
+			 * Iterate over each line.
+			 */
+			while ((lineReadFromBuffer = reader.readLine()) != null) {
+				
+				System.out.println(lineReadFromBuffer);
+			}
+
+			in.close();
+
+
+		} catch (Exception e) {
+			System.err.println("Unable to read file correctly:" + e.getMessage());
+
+
+		}
+
+
+
 		return 0; // To prevent an error in the project
 	}
 
-	/*
+	/**
 	 * Tries to match buy and sell orders. See assignment spec for more detail.
 	 */
 	public void exchange() {
 		// Implement this method
 	}
 
-	/*
+	/**
 	 * Returns a string of the buy and sell orders. See assignment spec for more
 	 * detail.
 	 */
@@ -44,7 +97,7 @@ public class Agent implements IAgent {
 		return ""; // To prevent an error in the project
 	}
 
-	/*
+	/**
 	 * Returns a string of the transactions. See assignment spec for more
 	 * detail.
 	 */
